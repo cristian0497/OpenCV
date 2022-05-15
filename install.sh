28c686724ce4dce9b8d975033c6d45688ba7207f
cristian0497 <cristian04.97@hotmail.com>
2022-05-15 18:41:01 -0500

    Complement Files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python3 --version
+
+sudo apt update
+sudo apt install python3-pip
+pip3 --version
+
+pip3 install opencv-python
+pip3 install matplotlib
+pip3 install numpy
+pip3 install scikit-image
+pip3 install scikit-learn
